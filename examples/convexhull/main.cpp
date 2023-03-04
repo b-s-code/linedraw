b1ccff24786b547177f349558429297b4ee34aa4
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-03-04 18:39:45 +0800

    add initial algo test but algo not implemented yet

--- examples/convexhull/main.cpp
+++ examples/convexhull/main.cpp
@@ -1,36 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <utility>
-
-class Edge
-{
-public:
-    Edge(int x1, int y1, int x2, int y2)
-    {
-        p1 = std::make_pair(x1, y1);
-        p2 = std::make_pair(x2, y2);
-    }
-    friend std::ostream& operator<<(std::ostream& os, const Edge& e);
-private:
-    std::pair<int, int> p1;
-    std::pair<int, int> p2;
-};
-
-std::ostream& operator<<(std::ostream& os, const Edge& e)
-{
-    os << e.p1.first << ',' << e.p1.second << ',' << e.p2.first << ',' << e.p2.second;
-    return os;
-}
+#include "hull.h"
 
 int main()
 {
-    std::vector<Edge> edges;
+    Hull::Point p (0, 0);
+    Hull::Point q (100, 100);
+    Hull::Point r (200, 0);
+    Hull::Point s (100, 50);
+    
+    std::vector<Hull::Point> points {p, q, r, s};
+    std::vector<Hull::Edge> edges = Hull::GetConvexHull(points);
 
-    Edge myDiagEdge = Edge(10,10,100,100);
-    edges.push_back(myDiagEdge);
-    edges.push_back(Edge(10,50,50,50));
-
-    for (Edge e : edges)
+    for (Hull::Edge e : edges)
     {
         std::cout << e << std::endl;
     }
